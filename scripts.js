f4ceee8301a9b1527ee91036a5920601f4aa7354
maha El-Feky <68131647+mahaElFeky@users.noreply.github.com>
2021-12-22 23:55:43 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ let prophetStatment;
 let alHadeeth;
 let source;
 
+
 function getQuotedBy(value) {
     quotedBy = value;
 
@@ -23,26 +24,22 @@ function getSource(value) {
     console.log("yuuygih");
 }
 
+
 function outputHadeeth() {
-    // console.log(quotedBy);
-    // console.log(prophetStatment);
-    // console.log(alHadeeth);
-    // console.log(source);
+
     document.getElementById("quotedBy").style.display = "none";
     document.getElementById("prophet").style.display = "none";
     document.getElementById("AlHadeeth").style.display = "none";
     document.getElementById("source").style.display = "none";
     document.getElementById("print").style.display = "none";
     document.getElementById("sourceValue").innerHTML = source;
-    // document.getElementById("sourceValue").style.textAlign = "center";
     document.getElementById("quotedByValue").innerHTML = quotedBy;
     document.getElementById("prophetValue").innerHTML = prophetStatment;
     document.getElementById("AlHadeethValue").innerHTML = alHadeeth;
-    //var container = document.getElementById("image-wrap"); //specific element on page
     let container = document.getElementById("page");
     html2canvas(container).then(function(canvas) {
-        document.getElementById("page").innerHTML = "";
-        document.getElementById("page").appendChild(canvas);
+        // container.innerHTML = "";
+        container.appendChild(canvas);
     });
 
 }
